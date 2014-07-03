f0a5c177dfbfc1366da445cd43962783c35ce9f8
Peter Vandenberghe <pvandenberghe@jetair.be>
2014-07-03 14:19:45 +0200

    Added date to scripts

--- testscript.cmd
+++ testscript.cmd
@@ -1,4 +1,4 @@
 @echo off
-echo Windows test script
-echo Nothing special to be found in here
-
+echo "Windows test script"
+echo "Nothing special to be found in here"
+echo "Updated: 2014-07-03 14:17"
--- testscript.sh
+++ testscript.sh
@@ -2,5 +2,6 @@
 
 echo "This is a test script."
 echo "There is nothing special in here."
+echo "Updated: 2014-07-03 14:17"
 exit 1
 
