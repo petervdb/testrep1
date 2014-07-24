80fd21d60b957aa39ebc8a801a1260ca9b8e6ee4
Peter Vandenberghe <petervanden@gmail.com>
2014-07-24 11:58:50 +0200

    Aliases updated

--- bin/updatedb.sh
+++ bin/updatedb.sh
@@ -1,1 +1,1 @@
-updatedb --prunepaths='/cygdrive/g /cygdrive/o /cygdrive/p /cygdrive/r'
+updatedb --prunepaths='/tmp /usr/tmp /var/tmp /afs /cygdrive/c /cygdrive/d /cygdrive/g /cygdrive/o /cygdrive/p /cygdrive/r'
