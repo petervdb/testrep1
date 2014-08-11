e699fdf6ac95a0c98b5c437cc92674aea44d179d
Peter Vandenberghe <petervanden@gmail.com>
2014-08-11 14:10:18 +0200

    Test scripts modified

--- test_scripts/testscript.cmd
+++ test_scripts/testscript.cmd
@@ -1,4 +1,7 @@
 @echo off
-echo "Windows test script"
-echo "Nothing special to be found in here"
-echo "Updated: 2014-07-03 14:17"
+echo Windows test script
+echo Nothing special to be found in here
+echo Updated: 2014-08-11 14:05
+echo Date and time of execution:
+date /t
+time /t
--- test_scripts/testscript.sh
+++ test_scripts/testscript.sh
@@ -2,6 +2,8 @@
 
 echo "This is a test script."
 echo "There is nothing special in here."
-echo "Updated: 2014-07-03 14:17"
+echo "Updated: 2014-08-11 14:07"
+echo "Date and time execution: "
+date '+%Y_%m_%d %H:%M:%S'
 exit 1
 
