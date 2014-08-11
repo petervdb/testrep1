85f3dc49b7a228b9afe7222746d5d8b1413241c4
Peter Vandenberghe <petervanden@gmail.com>
2014-08-11 14:25:16 +0200

    Show version info perl,PHP, python, ...

---
+++ test_scripts/show_version_info.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/bash
+# Show version information about different scripting tools used
+echo -n "Standard python version used : "
+python -V
+echo "================================================="
+echo -n "Python 3 version used        : "
+python3 -V
+echo "================================================="
+echo -n "Perl version used            : "
+perl -v
+echo "================================================="
+echo -n "Bash version used            : "
+bash --version
+echo "================================================="
+echo -n "PHP version used             : "
+php -v
+echo "================================================="
