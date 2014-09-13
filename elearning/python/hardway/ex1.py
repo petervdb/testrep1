f03cda85115b3da69bbd62790553336224d44ac7
Peter Vandenberghe <petervanden@gmail.com>
2014-09-13 14:23:01 +0200

    Ex 2 completed

---
+++ elearning/python/hardway/ex1.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python
+# Should work both with Python 2 and 3
+print("Hello World!")
+print("Hello Again") 
+print("I like typing this")
+print("This is fun")
+print("Yay! Printing")
+print("I'd much rather you not")
+print('I "said" do not touch this.')
---
+++ elearning/python/hardway/ex2.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python
+
+# A comment, this is so you can read your program later
+
+print("Blablabla ....");
+print ("Not exactly done what is mentioned in the book")
