a729f2f351de46d32586fc4aeec824fbc65db582
Peter Vandenberghe <petervanden@gmail.com>
2014-09-13 17:00:25 +0200

    Completed python exercise 3

---
+++ elearning/python/hardway/ex3.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python
+# Added some comments that are not included in the book.
+
+print "I will now count my chickens:"
+print "Hens", 25 + 30 / 6 # should be 30
+print "Roosters", 100 -25 * 3 % 4 # this is the same as 100 - 3
+print "Now I will count the eggs:"
+
+print 3 +2 + 1 - 5 + 4 % 2 - 1 / 4 +6 # same as 1 + 0 + 6
+print "It is true that 3 + 2 < 5 - 7?" # 5 < -2 ???
+print 3+ 2 < 5 - 7
+print "What is 3 + 2?", 3 + 2
+print "What is 5 - 7?", 5 - 7
+
+print "Oh, that's why it's False"
+
+print "How about some more."
+
+print "Is it greater?", 5 > -2
+print "Is it greater or equal?", 5 >= -2
+print "Is it less or equal?", 5 <= -2
+print ("======================================")
+print ("Now doing same stuff with floating points")
+print "I will now count my chickens:"
+print "Hens", 25.0 + 30.0 / 6.0 # should be 30
+print "Roosters", 100.0 - 25.0 * 3.0 % 4.0 # this is the same as 100 - 3
+print "Now I will count the eggs:"
+
+print 3 +2 + 1 - 5 + 4 % 2 - 1.0 / 4 +6 # same as 1 + 0 + 6
+print "It is true that 3 + 2 < 5 - 7?" # 5 < -2 ???
+print 3+ 2 < 5 - 7
+print "What is 3 + 2?", 3 + 2
+print "What is 5 - 7?", 5 - 7
+
+print "Oh, that's why it's False"
+
+print "How about some more."
+
+print "Is it greater?", 5 > -2
+print "Is it greater or equal?", 5 >= -2
+print "Is it less or equal?", 5 <= -2
