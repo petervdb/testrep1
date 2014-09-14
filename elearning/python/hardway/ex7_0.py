d908a4fa03bdf3c5bea38b856bded1c2f95d8955
Peter Vandenberghe <petervanden@gmail.com>
2014-09-14 20:24:29 +0200

    exercise 7 Python course completed

---
+++ elearning/python/hardway/ex7_0.py
@@ -0,0 +1,28 @@
+# Just print some boring stuff
+print "Mary had a little lamb."
+# strange stuff in here. And snow is just a string, not a variable
+print "Its fleece was white as %s." % 'snow'
+print "And everywhere that Mary went."
+# A lot of points
+print "." * 10 # what'd that do?
+
+# Lots of variables with characters
+end1 = "C"
+end2 = "h"
+end3 = "e"
+end4 = "e"
+end5 = "s"
+end6 = "e"
+end7 = "B"
+end8 = "u"
+end9 = "r"
+end10 = "g"
+end11 = "e"
+end12 = "r"
+
+# watch that comma at the end. try removing it to see what happens
+# It is considered bad style to place everything on 1 line that becomes longer than 80 characters
+
+print end1 + end2 + end3 + end4 + end5 + end6,
+# and were are not done yet
+print end7 + end8 + end9 + end10 + end11 + end12
