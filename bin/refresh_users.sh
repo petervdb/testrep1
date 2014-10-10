f337840c5073d7e4b41494a195662cb11852d932
Peter Vandenberghe <petervanden@gmail.com>
2014-10-10 15:49:23 +0200

    refresh_users.sh should only work in Cygwin environment

--- bin/refresh_users.sh
+++ bin/refresh_users.sh
@@ -1,9 +1,17 @@
 #!/usr/bin/bash
 # I use this script to update the /etc/passwd and /etc/group file with Active Directory
+# This script is only for my Cygwin environment
 #
 
 myscript=`basename $0`
 
+if [ $OSTYPE = "cygwin" ]
+then
+	echo "Working on Cygwin. Can continue."
+else 
+	echo "This is not a Cygwin environment. Exiting ..."
+	exit 1
+fi
 echo "Creating a backup first ..."
 # Lets make a backup first
 cur_date=`date '+%Y%m%d_%H%M%S'`
