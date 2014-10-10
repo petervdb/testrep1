f543b971c613648b14074235ab20c2afe780ec36
Peter Vandenberghe <petervanden@gmail.com>
2014-10-10 16:05:39 +0200

    Added update_all_github_repos.sh

---
+++ bin/update_all_github_repos.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/bash
+# I use this script to update all my GIT Repositories that are available on the running system
+#
+GIT_HOME=$HOME/GitHub
+echo "I will perform a pull request for the following repositories:"
+repos=`ls $GIT_HOME`
+ls -1 $GIT_HOME
+echo ""
+echo "Do you want me to continue? Y/N :"
+read check
+if [ $check = "Y" ]
+then
+	echo "Will start pulling these repositories"
+	for repository in $repos
+	do
+		echo "Performing git pull of $repository"
+		cd $GIT_HOME/$repository
+		git pull origin master
+		echo "Pull request completed"
+		echo "------------------------------------------------------------------------------------"
+	done
+	echo "Finished."
+else
+	echo "OK, exiting now ..."
+fi
