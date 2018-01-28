6e81a49547e2841fb784683e314926979456a345
Peter Vandenberghe <petervanden@gmail.com>
2018-01-28 19:29:53 +0100

    Updated incremental backup script

--- bin/create_incrementalbackup.sh
+++ bin/create_incrementalbackup.sh
@@ -23,7 +23,19 @@ if [ ! -f "${backup_dir}/srv_full_backup.tar.gz" ]; then
   echo "WARNING - No backup file found. You need to create a full backup first."
   exit 1;
 fi
-echo "INFO - Creating list file for incremental backups"
+echo "INFO - Creating incremental backups"
+# /etc folder
+echo "INFO - Creating incremental backup /etc folder."
+tar --listed-incremental=${backup_dir}/etc_snapshot.snar --level=1 -czpf ${backup_dir}/etc_incremental_backup.tar.gz /etc
+# /home folder
+echo "INFO - Creating incremental backup /home folder."
+tar --listed-incremental=${backup_dir}/home_snapshot.snar --level=0 -czpf ${backup_dir}/home_incremental_backup.tar.gz /home
+# /srv folder
+echo "INFO - Creating incremental backup /srv folder."
 tar --listed-incremental="${backup_dir}/srv_snapshot.snar" --level=1 -czpf "${backup_dir}/srv_incremental_backup.tar.gz" /srv
+# /var/lib folder
+echo "INFO - Creating incremental backup /var/lib folder."
+tar --listed-incremental=${backup_dir}/var_lib_snapshot.snar --level=0 -czpf ${backup_dir}/var_lib_incremental_backup.tar.gz /var/lib
+
 echo "INFO - Incremental backups created."
 
